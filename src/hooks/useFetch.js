4f1de196baf7ed86ecb22d4627e54f6006de2938
Virkkunen <vrkknn@vrkknn.net>
2023-01-20 14:52:49 -0300

    API

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export default function useFetch() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch('https://swapi.dev/api/planets');
+
+      if (!response.ok) {
+        const apiError = new Error(response.status);
+        apiError.response = response;
+        throw apiError;
+      }
+
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      setError(errors);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    isLoading,
+    errors,
+    fetchData,
+  };
+}
